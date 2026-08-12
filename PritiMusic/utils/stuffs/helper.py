d06a558e36852ab6e62e7c969171b1cb5cf54b91
istkharroohi78 <istkharroohi@gmail.com>
2026-08-12 23:40:37 +0530

    Update helper.py

--- PritiMusic/utils/stuffs/helper.py
+++ PritiMusic/utils/stuffs/helper.py
@@ -1,231 +1,232 @@
 # helper for strings
 
 class Helper(object):
-    HELP_M = '''ᴄʜᴏᴏsᴇ ᴛʜᴇ ᴄᴀᴛᴇɢᴏʀʏ ғᴏʀ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴɴᴀ ɢᴇᴛ ʜᴇʟᴩ.
-ᴀsᴋ ʏᴏᴜʀ ᴅᴏᴜʙᴛs ᴀᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ
+    HELP_M = '''<tg-emoji emoji-id="5260512129240276089">📚</tg-emoji> ᴄʜᴏᴏsᴇ ᴛʜᴇ ᴄᴀᴛᴇɢᴏʀʏ ғᴏʀ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴɴᴀ ɢᴇᴛ ʜᴇʟᴩ.
+<tg-emoji emoji-id="5188540541922480562">❓</tg-emoji> ᴀsᴋ ʏᴏᴜʀ ᴅᴏᴜʙᴛs ᴀᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ
 
-ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /'''
-    HELP_ChatGPT = '''CʜᴀᴛGPT
+<tg-emoji emoji-id="6172663483834831848">⌨️</tg-emoji> ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /'''
+    
+    HELP_ChatGPT = '''<tg-emoji emoji-id="5355051922862653659">🤖</tg-emoji> <b>CʜᴀᴛGPT</b>
 
-CʜᴀᴛGPT ᴄᴏᴍᴍᴀɴᴅꜱ:
+<tg-emoji emoji-id="5409368076447657845">🌟</tg-emoji> CʜᴀᴛGPT ᴄᴏᴍᴍᴀɴᴅꜱ:
 
-/ask ➠ ǫᴜᴇʀɪᴇs ᴛʜᴇ ᴀɪ ᴍᴏᴅᴇʟ ᴛᴏ ɢᴇᴛ ᴀ ʀᴇsᴘᴏɴsᴇ ᴛᴏ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /ask ➠ ǫᴜᴇʀɪᴇs ᴛʜᴇ ᴀɪ ᴍᴏᴅᴇʟ ᴛᴏ ɢᴇᴛ ᴀ ʀᴇsᴘᴏɴsᴇ ᴛᴏ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ.
 '''
 
-    HELP_Sticker = '''sᴛɪᴄᴋᴇʀs
+    HELP_Sticker = '''<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> <b>sᴛɪᴄᴋᴇʀs</b>
 
-sᴛɪᴄᴋᴇʀs ᴄᴏᴍᴍᴀɴᴅꜱ:
+<tg-emoji emoji-id="5409143496902716934">🖼</tg-emoji> sᴛɪᴄᴋᴇʀs ᴄᴏᴍᴍᴀɴᴅꜱ:
 
-/packkang ➠ ᴄʀᴇᴀᴛᴇs ᴀ ᴘᴀᴄᴋ ᴏғ sᴛɪᴄᴋᴇʀs ғʀᴏᴍ ᴀ ᴏᴛʜᴇʀ ᴘᴀᴄᴋ.
-/stickerid ➠ ɢᴇᴛs ᴛʜᴇ sᴛɪᴄᴋᴇʀ ɪᴅ ᴏғ ᴀ sᴛɪᴄᴋᴇʀ.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /packkang ➠ ᴄʀᴇᴀᴛᴇs ᴀ ᴘᴀᴄᴋ ᴏғ sᴛɪᴄᴋᴇʀs ғʀᴏᴍ ᴀ ᴏᴛʜᴇʀ ᴘᴀᴄᴋ.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /stickerid ➠ ɢᴇᴛs ᴛʜᴇ sᴛɪᴄᴋᴇʀ ɪᴅ ᴏғ ᴀ sᴛɪᴄᴋᴇʀ.
 '''
 
-    HELP_TagAll = '''Tᴀɢ
+    HELP_TagAll = '''<tg-emoji emoji-id="6260059243605398502">🏷</tg-emoji> <b>Tᴀɢ</b>
 
-  Tᴀɢ ᴄᴏᴍᴍᴀɴᴅꜱ:
+<tg-emoji emoji-id="6039381989985882045">📢</tg-emoji> Tᴀɢ ᴄᴏᴍᴍᴀɴᴅꜱ:
 
-✿ ᴄʜᴏᴏsᴇ ᴛᴀɢ ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ ✿
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> ᴄʜᴏᴏsᴇ ᴛᴀɢ ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ <tg-emoji emoji-id="6172312314423808834">✨</tg-emoji>
 
-๏ /gmtag ➛ ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ 
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /gmtag ➛ ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ 
 ᴛᴀɢ sᴛᴏᴘ ⇴ /gmstop
 
-๏ /gntag ➛ ɢᴏᴏᴅ ɴɪɢʜᴛ ᴛᴀɢ sᴛᴏᴘ ⇴ /gnstop
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /gntag ➛ ɢᴏᴏᴅ ɴɪɢʜᴛ ᴛᴀɢ sᴛᴏᴘ ⇴ /gnstop
 
-๏ /tagall ➛ ʀᴀɴᴅᴏᴍ ᴍᴇssᴀɢᴇ ᴛᴀɢ sᴛᴏᴘ ⇴ /tagoff /tagstop
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /tagall ➛ ʀᴀɴᴅᴏᴍ ᴍᴇssᴀɢᴇ ᴛᴀɢ sᴛᴏᴘ ⇴ /tagoff /tagstop
 
-๏ /hitag ➛ ʀᴀɴᴅᴏᴍ ʜɪɴᴅɪ ᴍᴇssᴀɢᴇ ᴛᴀɢ sᴛᴏᴘ ⇴/histop
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /hitag ➛ ʀᴀɴᴅᴏᴍ ʜɪɴᴅɪ ᴍᴇssᴀɢᴇ ᴛᴀɢ sᴛᴏᴘ ⇴/histop
 
-๏ /shayari ➛ ʀᴀɴᴅᴏᴍ sʜᴀʏᴀʀɪ ᴛᴀɢ sᴛᴏᴘ ⇴ /shstop
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /shayari ➛ ʀᴀɴᴅᴏᴍ sʜᴀʏᴀʀɪ ᴛᴀɢ sᴛᴏᴘ ⇴ /shstop
 
-๏ /utag ➛ ᴀɴʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ ᴛᴀɢ sᴛᴏᴘ ⇴ /cancel 
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /utag ➛ ᴀɴʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ ᴛᴀɢ sᴛᴏᴘ ⇴ /cancel 
 
-๏ /vctag ➛ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ɪɴᴠɪᴛᴇ ᴛᴀɢ sᴛᴏᴘ ⇴ /vcstop
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /vctag ➛ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ɪɴᴠɪᴛᴇ ᴛᴀɢ sᴛᴏᴘ ⇴ /vcstop
 '''
 
-    HELP_Info = '''Iɴꜰᴏ
+    HELP_Info = '''<tg-emoji emoji-id="5188540541922480562">❓</tg-emoji> <b>Iɴꜰᴏ</b>
 
-Iɴꜰᴏ ᴄᴏᴍᴍᴀɴᴅꜱ:
+<tg-emoji emoji-id="5429571366384842791">🔎</tg-emoji> Iɴꜰᴏ ᴄᴏᴍᴍᴀɴᴅꜱ:
 
-/id : ɢᴇᴛ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ɢʀᴏᴜᴘ ɪᴅ. ɪғ ᴜsᴇᴅ ʙʏ ʀᴇᴘʟʏɪɴɢ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ, ɢᴇᴛs ᴛʜᴀᴛ ᴜsᴇʀ's ɪᴅ.
-/info : ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴀ ᴜsᴇʀ.
-/github <ᴜsᴇʀɴᴀᴍᴇ> : ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴀ ɢɪᴛʜᴜʙ ᴜsᴇʀ.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /id : ɢᴇᴛ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ɢʀᴏᴜᴘ ɪᴅ. ɪғ ᴜsᴇᴅ ʙʏ ʀᴇᴘʟʏɪɴɢ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ, ɢᴇᴛs ᴛʜᴀᴛ ᴜsᴇʀ's ɪᴅ.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /info : ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴀ ᴜsᴇʀ.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /github <ᴜsᴇʀɴᴀᴍᴇ> : ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴀ ɢɪᴛʜᴜʙ ᴜsᴇʀ.
 '''
-    HELP_Group = '''Gʀᴏᴜᴘ
+    HELP_Group = '''<tg-emoji emoji-id="6032609071373226027">👥</tg-emoji> <b>Gʀᴏᴜᴘ</b>
 
-Gʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅꜱ:
+<tg-emoji emoji-id="5350396951407895212">⚙️</tg-emoji> Gʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅꜱ:
 
 ᴛʜᴇsᴇ ᴀʀᴇ ᴛʜᴇ ᴀᴠᴀɪʟᴀʙʟᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ᴄᴏᴍᴍᴀɴᴅs:
 
-⦿ /pin ➠ ᴘɪɴs ᴀ ᴍᴇssᴀɢᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ.
-⦿ /pinned ➠ ᴅɪsᴘʟᴀʏs ᴛʜᴇ ᴘɪɴɴᴇᴅ ᴍᴇssᴀɢᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ.
-⦿ /unpin ➠ ᴜɴᴘɪɴs ᴛʜᴇ ᴄᴜʀʀᴇɴᴛʟʏ ᴘɪɴɴᴇᴅ ᴍᴇssᴀɢᴇ.
-⦿ /staff ➠ ᴅɪsᴘʟᴀʏs ᴛʜᴇ ʟɪsᴛ ᴏғ sᴛᴀғғ ᴍᴇᴍʙᴇʀs.
-⦿ /bots ➠ ᴅɪsᴘʟᴀʏs ᴛʜᴇ ʟɪsᴛ ᴏғ ʙᴏᴛs ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ.
-⦿ /settitle ➠ sᴇᴛs ᴛʜᴇ ᴛɪᴛʟᴇ ᴏғ ᴛʜᴇ ɢʀᴏᴜᴘ.
-⦿ /setdiscription ➠ sᴇᴛs ᴛʜᴇ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ᴏғ ᴛʜᴇ ɢʀᴏᴜᴘ.
-⦿ /setphoto ➠ sᴇᴛs ᴛʜᴇ ɢʀᴏᴜᴘ ᴘʜᴏᴛᴏ.
-⦿ /removephoto ➠ ʀᴇᴍᴏᴠᴇs ᴛʜᴇ ɢʀᴏᴜᴘ ᴘʜᴏᴛᴏ.
-⦿ /zombies ➠ ʀᴇᴍᴏᴠᴇs ᴀᴄᴄ ᴅᴇʟᴇᴛᴇᴅ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴛʜᴇ ɢʀᴏᴜᴘ.
-⦿ /imposter ᴏɴ/ᴏғғ ➠ ᴛᴜʀɴs ᴏɴ ᴏʀ ᴏғғ ᴛʜᴇ ᴡᴀᴛᴄʜᴇʀ ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ, ᴡʜɪᴄʜ ɴᴏᴛɪғɪᴇs ᴀʙᴏᴜᴛ ᴜsᴇʀs ᴡʜᴏ ᴄʜᴀɴɢᴇ ᴛʜᴇɪʀ ɴᴀᴍᴇ ᴏʀ ᴜsᴇʀɴᴀᴍᴇ.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /pin ➠ ᴘɪɴs ᴀ ᴍᴇssᴀɢᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /pinned ➠ ᴅɪsᴘʟᴀʏs ᴛʜᴇ ᴘɪɴɴᴇᴅ ᴍᴇssᴀɢᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /unpin ➠ ᴜɴᴘɪɴs ᴛʜᴇ ᴄᴜʀʀᴇɴᴛʟʏ ᴘɪɴɴᴇᴅ ᴍᴇssᴀɢᴇ.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /staff ➠ ᴅɪsᴘʟᴀʏs ᴛʜᴇ ʟɪsᴛ ᴏғ sᴛᴀғғ ᴍᴇᴍʙᴇʀs.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /bots ➠ ᴅɪsᴘʟᴀʏs ᴛʜᴇ ʟɪsᴛ ᴏғ ʙᴏᴛs ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /settitle ➠ sᴇᴛs ᴛʜᴇ ᴛɪᴛʟᴇ ᴏғ ᴛʜᴇ ɢʀᴏᴜᴘ.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /setdiscription ➠ sᴇᴛs ᴛʜᴇ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ᴏғ ᴛʜᴇ ɢʀᴏᴜᴘ.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /setphoto ➠ sᴇᴛs ᴛʜᴇ ɢʀᴏᴜᴘ ᴘʜᴏᴛᴏ.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /removephoto ➠ ʀᴇᴍᴏᴠᴇs ᴛʜᴇ ɢʀᴏᴜᴘ ᴘʜᴏᴛᴏ.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /zombies ➠ ʀᴇᴍᴏᴠᴇs ᴀᴄᴄ ᴅᴇʟᴇᴛᴇᴅ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴛʜᴇ ɢʀᴏᴜᴘ.
+<tg-emoji emoji-id="6280269890821558384">✅</tg-emoji> /imposter ᴏɴ/ᴏғғ ➠ ᴛᴜʀɴs ᴏɴ ᴏʀ ᴏғғ ᴛʜᴇ ᴡᴀᴛᴄʜᴇʀ ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ, ᴡʜɪᴄʜ ɴᴏᴛɪғɪᴇs ᴀʙᴏᴜᴛ ᴜsᴇʀs ᴡʜᴏ ᴄʜᴀɴɢᴇ ᴛʜᴇɪʀ ɴᴀᴍᴇ ᴏʀ ᴜsᴇʀɴᴀᴍᴇ.
 '''
 
-    HELP_Extra = '''Exᴛʀᴀ
+    HELP_Extra = '''<tg-emoji emoji-id="5408843502027033965">📦</tg-emoji> <b>Exᴛʀᴀ</b>
 
-Exᴛʀᴀ ᴄᴏᴍᴍᴀɴᴅꜱ:
+<tg-emoji emoji-id="5767288287001580715">💡</tg-emoji> Exᴛʀᴀ ᴄᴏᴍᴍᴀɴᴅꜱ:
 
-⦿ /math ➠ sᴏʟᴠᴇs ᴍᴀᴛʜᴇᴍᴀᴛɪᴄᴀʟ ᴘʀᴏʙʟᴇᴍs ᴀɴᴅ ᴇǫᴜᴀᴛɪᴏɴs.
-⦿ /blackpink ➠ ɢᴇɴᴇʀᴀᴛᴇs ᴀ ʙʟᴀᴄᴋᴘɪɴᴋ-sᴛʏʟᴇ ʟᴏɢᴏ.
-⦿ /carbon ➠ ɢᴇɴᴇʀᴀᴛᴇs ᴀ ᴄᴀʀʙᴏɴ ᴄᴏᴅᴇ ɪᴍᴀɢᴇ ғʀᴏᴍ ᴀ ᴄᴏᴅᴇ sɴɪᴘᴘᴇᴛ.
-⦿ /speedtest ➠ ᴍᴇᴀsᴜʀᴇs ᴛʜᴇ ɪɴᴛᴇʀɴᴇᴛ sᴘᴇᴇᴅ.
-⦿ /reverse ➠ ʀᴇᴠᴇʀsᴇs ᴀ ɢɪᴠᴇɴ ᴛᴇxᴛ.
-⦿ /webss ➠ ᴛᴀᴋᴇs ᴀ sᴄʀᴇᴇɴsʜᴏᴛ ᴏғ ᴀ ᴡᴇʙsɪᴛᴇ.
-⦿ /paste ➠ ᴜᴘʟᴏᴀᴅs ᴀ ᴛᴇxᴛ sɴɪᴘᴘᴇᴛ ᴛᴏ ᴛʜᴇ ᴄʟᴏᴜᴅ ᴀɴᴅ ɢɪᴠᴇs ᴀ ʟɪɴᴋ.
-⦿ /tgm ➠ ᴜᴘʟᴏᴀᴅs ᴀ ᴘʜᴏᴛᴏ (ᴜɴᴅᴇʀ 𝟻ᴍʙ) ᴛᴏ ᴛʜᴇ ᴄʟᴏᴜᴅ ᴀɴᴅ ɢɪᴠᴇs ᴀ ʟɪɴᴋ.
-⦿ /tr ➠ ᴛʀᴀɴsʟᴀᴛᴇs ᴛᴇxᴛ.
-⦿ /google ➠ sᴇᴀʀᴄʜᴇs ғᴏʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴏɴ ɢᴏᴏɢʟᴇ.
-⦿ /stack ➠ sᴇᴀʀᴄʜᴇs ғᴏʀ ᴘʀᴏɢʀᴀᴍᴍɪɴɢ-ʀᴇʟᴀᴛᴇᴅ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴏɴ sᴛᴀᴄᴋ ᴏᴠᴇʀғʟᴏᴡ.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /math ➠ sᴏʟᴠᴇs ᴍᴀᴛʜᴇᴍᴀᴛɪᴄᴀʟ ᴘʀᴏʙʟᴇᴍs ᴀɴᴅ ᴇǫᴜᴀᴛɪᴏɴs.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /blackpink ➠ ɢᴇɴᴇʀᴀᴛᴇs ᴀ ʙʟᴀᴄᴋᴘɪɴᴋ-sᴛʏʟᴇ ʟᴏɢᴏ.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /carbon ➠ ɢᴇɴᴇʀᴀᴛᴇs ᴀ ᴄᴀʀʙᴏɴ ᴄᴏᴅᴇ ɪᴍᴀɢᴇ ғʀᴏᴍ ᴀ ᴄᴏᴅᴇ sɴɪᴘᴘᴇᴛ.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /speedtest ➠ ᴍᴇᴀsᴜʀᴇs ᴛʜᴇ ɪɴᴛᴇʀɴᴇᴛ sᴘᴇᴇᴅ.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /reverse ➠ ʀᴇᴠᴇʀsᴇs ᴀ ɢɪᴠᴇɴ ᴛᴇxᴛ.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /webss ➠ ᴛᴀᴋᴇs ᴀ sᴄʀᴇᴇɴsʜᴏᴛ ᴏғ ᴀ ᴡᴇʙsɪᴛᴇ.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /paste ➠ ᴜᴘʟᴏᴀᴅs ᴀ ᴛᴇxᴛ sɴɪᴘᴘᴇᴛ ᴛᴏ ᴛʜᴇ ᴄʟᴏᴜᴅ ᴀɴᴅ ɢɪᴠᴇs ᴀ ʟɪɴᴋ.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /tgm ➠ ᴜᴘʟᴏᴀᴅs ᴀ ᴘʜᴏᴛᴏ (ᴜɴᴅᴇʀ 𝟻ᴍʙ) ᴛᴏ ᴛʜᴇ ᴄʟᴏᴜᴅ ᴀɴᴅ ɢɪᴠᴇs ᴀ ʟɪɴᴋ.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /tr ➠ ᴛʀᴀɴsʟᴀᴛᴇs ᴛᴇxᴛ.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /google ➠ sᴇᴀʀᴄʜᴇs ғᴏʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴏɴ ɢᴏᴏɢʟᴇ.
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /stack ➠ sᴇᴀʀᴄʜᴇs ғᴏʀ ᴘʀᴏɢʀᴀᴍᴍɪɴɢ-ʀᴇʟᴀᴛᴇᴅ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴏɴ sᴛᴀᴄᴋ ᴏᴠᴇʀғʟᴏᴡ.
 '''
 
-    HELP_Image = '''Iᴍᴀɢᴇ
+    HELP_Image = '''<tg-emoji emoji-id="5409143496902716934">🖼</tg-emoji> <b>Iᴍᴀɢᴇ</b>
 
-Iᴍᴀɢᴇ ᴄᴏᴍᴍᴀɴᴅꜱ:
+<tg-emoji emoji-id="5767117162619605573">📷</tg-emoji> Iᴍᴀɢᴇ ᴄᴏᴍᴍᴀɴᴅꜱ:
 
-⦿ /draw ➠ ɢᴇɴᴇʀᴀᴛᴇs ᴀ ᴅʀᴀᴡɪɴɢ ʙᴀsᴇᴅ ᴏɴ ᴀ ɢɪᴠᴇɴ ᴘᴏʀᴏᴍᴘᴛ.
-⦿ /image ➠ sᴇᴀʀᴄʜᴇs ғᴏʀ ᴀɴ ɪᴍᴀɢᴇ ʙᴀsᴇᴅ ᴏɴ ᴀ ɢɪᴠᴇɴ ᴋᴇʏᴡᴏʀᴅ.
-⦿ /upscale ➠ ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ɪᴛ ᴀɴᴅ ɪᴍᴘʀᴏᴠᴇ ɪᴛs ǫᴜᴀʟɪᴛʏ.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /draw ➠ ɢᴇɴᴇʀᴀᴛᴇs ᴀ ᴅʀᴀᴡɪɴɢ ʙᴀsᴇᴅ ᴏɴ ᴀ ɢɪᴠᴇɴ ᴘᴏʀᴏᴍᴘᴛ.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /image ➠ sᴇᴀʀᴄʜᴇs ғᴏʀ ᴀɴ ɪᴍᴀɢᴇ ʙᴀsᴇᴅ ᴏɴ ᴀ ɢɪᴠᴇɴ ᴋᴇʏᴡᴏʀᴅ.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /upscale ➠ ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ɪᴛ ᴀɴᴅ ɪᴍᴘʀᴏᴠᴇ ɪᴛs ǫᴜᴀʟɪᴛʏ.
 '''
-    HELP_Action = '''Aᴄᴛɪᴏɴ
+    HELP_Action = '''<tg-emoji emoji-id="6271824284310573725">👮‍♂️</tg-emoji> <b>Aᴄᴛɪᴏɴ</b>
 
-Aᴄᴛɪᴏɴ ᴄᴏᴍᴍᴀɴᴅꜱ:
+<tg-emoji emoji-id="5275969776668134187">⛔️</tg-emoji> Aᴄᴛɪᴏɴ ᴄᴏᴍᴍᴀɴᴅꜱ:
 
 » ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs ꜰᴏʀ Bᴀɴs & Mᴜᴛᴇ :
 
- ❍ /kickme: kicks the user who issued the command
+ <tg-emoji emoji-id="6100397162976252509">🚫</tg-emoji> /kickme: kicks the user who issued the command
 
 Admins only:
- ❍ /ban <userhandle>: bans a user. (via handle, or reply)
- ❍ /sban <userhandle>: Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
- ❍ /tban <userhandle> x(m/h/d): bans a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
- ❍ /unban <userhandle>: unbans a user. (via handle, or reply)
- ❍ /kick <userhandle>: kicks a user out of the group, (via handle, or reply)
- ❍ /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
- ❍ /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
- ❍ /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
+ <tg-emoji emoji-id="6100397162976252509">🚫</tg-emoji> /ban <userhandle>: bans a user. (via handle, or reply)
+ <tg-emoji emoji-id="6100397162976252509">🚫</tg-emoji> /sban <userhandle>: Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
+ <tg-emoji emoji-id="6100397162976252509">🚫</tg-emoji> /tban <userhandle> x(m/h/d): bans a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
+ <tg-emoji emoji-id="6100397162976252509">🚫</tg-emoji> /unban <userhandle>: unbans a user. (via handle, or reply)
+ <tg-emoji emoji-id="6100397162976252509">🚫</tg-emoji> /kick <userhandle>: kicks a user out of the group, (via handle, or reply)
+ <tg-emoji emoji-id="6100397162976252509">🚫</tg-emoji> /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
+ <tg-emoji emoji-id="6100397162976252509">🚫</tg-emoji> /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
+ <tg-emoji emoji-id="6100397162976252509">🚫</tg-emoji> /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
 __
 𝐒ᴘᴇᴄɪᴀʟ 𝐂ᴏᴍᴍᴀɴᴅs 𝐒ᴜᴘᴘᴏʀᴛ 𝐀ʟʟ 𝐄xᴀᴍᴘʟᴇ  - 𝚈𝚞𝚖𝚒 𝚋𝚊𝚗 𝚈𝚞𝚖𝚒 𝚖𝚞𝚝𝚎 𝚈𝚞𝚖𝚒 𝚙𝚛𝚘𝚖𝚘𝚝𝚎 ..... 𝚎𝚝𝚌
 '''
-    HELP_Search = '''Sᴇᴀʀᴄʜ
+    HELP_Search = '''<tg-emoji emoji-id="5429571366384842791">🔎</tg-emoji> <b>Sᴇᴀʀᴄʜ</b>
 
-Sᴇᴀʀᴄʜ ᴄᴏᴍᴍᴀɴᴅꜱ:
+<tg-emoji emoji-id="6269490656779965144">🌐</tg-emoji> Sᴇᴀʀᴄʜ ᴄᴏᴍᴍᴀɴᴅꜱ:
 
-• /google <query> : Search the google for the given query.
-• /anime <query>  : Search myanimelist for the given query.
-• /stack <query>  : Search stackoverflow for the given query.
-• /image (/imgs) <query> : Get the images regarding to your query
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /google <query> : Search the google for the given query.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /anime <query>  : Search myanimelist for the given query.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /stack <query>  : Search stackoverflow for the given query.
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /image (/imgs) <query> : Get the images regarding to your query
 
 Example:
 /google pyrogram: return top 5 reuslts.
 '''
 
-    HELP_Font = '''ғᴏɴᴛ
+    HELP_Font = '''<tg-emoji emoji-id="5370546867786523009">📝</tg-emoji> <b>ғᴏɴᴛ</b>
 
 ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴛʜᴇ ғᴏɴᴛ ᴍᴏᴅᴜʟᴇ:
 
-ғᴏɴᴛ ᴍᴏᴅᴜʟᴇ:
+<tg-emoji emoji-id="5370546867786523009">📝</tg-emoji> ғᴏɴᴛ ᴍᴏᴅᴜʟᴇ:
 
 ʙʏ ᴜsɪɴɢ ᴛʜɪs ᴍᴏᴅᴜʟᴇ ʏᴏᴜ ᴄᴀɴ ᴄʜᴀɴɢᴇ ғᴏɴᴛs ᴏғ ᴀɴʏ ᴛᴇxᴛ!
 
-◌ /font [ᴛᴇxᴛ]
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> /font [ᴛᴇxᴛ]
 '''
-    HELP_Game = '''ɢᴀᴍᴇs
+    HELP_Game = '''<tg-emoji emoji-id="6246741653827095091">🏓</tg-emoji> <b>ɢᴀᴍᴇs</b>
 
 ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴛʜᴇ ɢᴀᴍᴇs ᴍᴏᴅᴜʟᴇ:
-ɢᴀᴍᴇs ᴍᴏᴅᴜʟᴇ:
+<tg-emoji emoji-id="6246741653827095091">🏓</tg-emoji> ɢᴀᴍᴇs ᴍᴏᴅᴜʟᴇ:
 
 ʜᴇʀᴇ ᴀʀᴇ sᴏᴍᴇ ᴍɪɴɪ ɢᴀᴍᴇs ғᴏʀ ʏᴏᴜ ᴛᴏ ᴘʟᴀʏ!
 
-◌ /toss [ᴛᴏss ᴀ ᴄᴏɪɴ]
+<tg-emoji emoji-id="5409368076447657845">🌟</tg-emoji> /toss [ᴛᴏss ᴀ ᴄᴏɪɴ]
 
-◌ /roll [ʀᴏʟʟ ᴀ ᴅɪᴄᴇ]
+<tg-emoji emoji-id="5409368076447657845">🌟</tg-emoji> /roll [ʀᴏʟʟ ᴀ ᴅɪᴄᴇ]
 
-◌ /dart [ᴛʜʀᴏᴡ ᴀ ᴅᴀʀᴛ]
+<tg-emoji emoji-id="5409368076447657845">🌟</tg-emoji> /dart [ᴛʜʀᴏᴡ ᴀ ᴅᴀʀᴛ]
 
-◌ /slot [Jᴀᴄᴋᴘᴏᴛ ᴍᴀᴄʜɪɴᴇ]
+<tg-emoji emoji-id="5409368076447657845">🌟</tg-emoji> /slot [Jᴀᴄᴋᴘᴏᴛ ᴍᴀᴄʜɪɴᴇ]
 
-◌ /bowling [ʙᴏᴡʟɪɴɢ ɢᴀᴍᴇ]
+<tg-emoji emoji-id="5409368076447657845">🌟</tg-emoji> /bowling [ʙᴏᴡʟɪɴɢ ɢᴀᴍᴇ]
 
-◌ /basket [ʙᴀsᴋᴇᴛʙᴀʟʟ ɢᴀᴍᴇ]
+<tg-emoji emoji-id="5409368076447657845">🌟</tg-emoji> /basket [ʙᴀsᴋᴇᴛʙᴀʟʟ ɢᴀᴍᴇ]
 
-◌ /football [ғᴏᴏᴛʙᴀʟʟ ɢᴀᴍᴇ]
+<tg-emoji emoji-id="5409368076447657845">🌟</tg-emoji> /football [ғᴏᴏᴛʙᴀʟʟ ɢᴀᴍᴇ]
 '''
-    HELP_TG = '''Ⓣ-ɢʀᴀᴘʜ
+    HELP_TG = '''<tg-emoji emoji-id="5778455936410588193">🔗</tg-emoji> <b>Ⓣ-ɢʀᴀᴘʜ</b>
 
-Ⓣ-ɢʀᴀᴘʜ ᴄᴏᴍᴍᴀɴᴅꜱ:
+<tg-emoji emoji-id="5778455936410588193">🔗</tg-emoji> Ⓣ-ɢʀᴀᴘʜ ᴄᴏᴍᴍᴀɴᴅꜱ:
 
 ᴄʀᴇᴀᴛᴇ ᴀ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ ᴀɴʏ ᴍᴇᴅɪᴀ!
 
-◌ /tgm [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇᴅɪᴀ]
-◌ /tgt [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇᴅɪᴀ]
+<tg-emoji emoji-id="5409143496902716934">🖼</tg-emoji> /tgm [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇᴅɪᴀ]
+<tg-emoji emoji-id="5409143496902716934">🖼</tg-emoji> /tgt [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇᴅɪᴀ]
 '''
-    HELP_Imposter = '''ɪᴍᴘᴏsᴛᴇʀ
+    HELP_Imposter = '''<tg-emoji emoji-id="6100546468924364734">👀</tg-emoji> <b>ɪᴍᴘᴏsᴛᴇʀ</b>
 
 ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴛʜᴇ ɪᴍᴘᴏsᴛᴇʀ ᴍᴏᴅᴜʟᴇ:
 
-ɪᴍᴘᴏsᴛᴇʀ ᴍᴏᴅᴜʟᴇ:
+<tg-emoji emoji-id="6100546468924364734">👀</tg-emoji> ɪᴍᴘᴏsᴛᴇʀ ᴍᴏᴅᴜʟᴇ:
 
-◌ /imposter on
-◌ /imposter off
+<tg-emoji emoji-id="5429571366384842791">🔎</tg-emoji> /imposter on
+<tg-emoji emoji-id="5429571366384842791">🔎</tg-emoji> /imposter off
 '''
-    HELP_TD = '''Tʀᴜᴛʜ-ᗪᴀʀᴇ
+    HELP_TD = '''<tg-emoji emoji-id="5373310679241466020">🌀</tg-emoji> <b>Tʀᴜᴛʜ-ᗪᴀʀᴇ</b>
 
 ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴛʜᴇ Tʀᴜᴛʜ-ᗪᴀʀᴇ ᴍᴏᴅᴜʟᴇ:
 
-ᴛʀᴜᴛʜ ᴀɴᴅ ᴅᴀʀᴇ
-◌ /truth : sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴛʀᴜᴛʜ sᴛʀɪɴɢ.
-◌ /dare : sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴅᴀʀᴇ sᴛʀɪɴɢ.
+<tg-emoji emoji-id="5373310679241466020">🌀</tg-emoji> ᴛʀᴜᴛʜ ᴀɴᴅ ᴅᴀʀᴇ
+<tg-emoji emoji-id="5188540541922480562">❓</tg-emoji> /truth : sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴛʀᴜᴛʜ sᴛʀɪɴɢ.
+<tg-emoji emoji-id="5188540541922480562">❓</tg-emoji> /dare : sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴅᴀʀᴇ sᴛʀɪɴɢ.
 '''
-    HELP_HT = '''ʜᴀsᴛᴀɢ
+    HELP_HT = '''<tg-emoji emoji-id="6260059243605398502">🏷</tg-emoji> <b>ʜᴀsᴛᴀɢ</b>
 
 ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴛʜᴇ ʜᴀsᴛᴀɢ ᴍᴏᴅᴜʟᴇ:
 
-ʜᴀsᴛᴀɢ
-◌ /hastag : [ᴛᴇxᴛ]
+<tg-emoji emoji-id="6260059243605398502">🏷</tg-emoji> ʜᴀsᴛᴀɢ
+<tg-emoji emoji-id="6100424015111787987">📌</tg-emoji> /hastag : [ᴛᴇxᴛ]
 '''
-    HELP_TTS = '''ᴛᴛs
+    HELP_TTS = '''<tg-emoji emoji-id="5409025823388741707">🎵</tg-emoji> <b>ᴛᴛs</b>
 
 ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴛʜᴇ ᴛᴛs ᴍᴏᴅᴜʟᴇ:
 
 ❀ ᴛᴛs
-◌ /tts : [ᴛᴇxᴛ]
+<tg-emoji emoji-id="6082387600599944892">🎧</tg-emoji> /tts : [ᴛᴇxᴛ]
 
-◌ ᴜsᴀɢᴇ ➛ ᴛᴇxᴛ ᴛᴏ ᴀᴜᴅɪᴏ
+<tg-emoji emoji-id="5767288287001580715">💡</tg-emoji> ᴜsᴀɢᴇ ➛ ᴛᴇxᴛ ᴛᴏ ᴀᴜᴅɪᴏ
 '''
-    HELP_Fun = '''ғᴜɴ
+    HELP_Fun = '''<tg-emoji emoji-id="6172273586703700991">🥀</tg-emoji> <b>ғᴜɴ</b>
 
 ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴛʜᴇ ғᴜɴ ᴍᴏᴅᴜʟᴇ:
-ᴡɪsʜ ᴍᴏᴅᴜʟᴇ:
+<tg-emoji emoji-id="6172312314423808834">✨</tg-emoji> ᴡɪsʜ ᴍᴏᴅᴜʟᴇ:
 
-◌ /wish : ᴀᴅᴅ ʏᴏᴜʀ ᴡɪsʜ ᴀɴᴅ sᴇᴇ ɪᴛs ᴘᴏssɪʙɪʟɪᴛʏ!
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /wish : ᴀᴅᴅ ʏᴏᴜʀ ᴡɪsʜ ᴀɴᴅ sᴇᴇ ɪᴛs ᴘᴏssɪʙɪʟɪᴛʏ!
 
 ᴍᴏʀᴇ sᴛᴜғғ:
-◌ /sigma [ᴄʜᴇᴄᴋ ʏᴏᴜʀ sɪɢᴍᴀɴᴇss]
-◌ /cute [ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜᴛᴇɴᴇss]
-◌ /horny [ᴄʜᴇᴄᴋ ʏᴏᴜʀ ʜᴏʀɴʏɴᴇss]
-◌ /lesbo [ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ʟᴇᴢʙɪᴀɴ ʏᴏᴜ ᴀʀᴇ]
-◌ /depressed [ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ᴅᴇᴘʀᴇssᴇᴅ ʏᴏᴜ ᴀʀᴇ]
-◌ /gay [ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ɢᴀʏ ʏᴏᴜ ᴀʀᴇ]
-◌ /rand [ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ʀᴀɴᴅ ʏᴏᴜ ᴀʀᴇ]
-◌ /bkl [ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ʙᴋʟ ʏᴏᴜ ᴀʀᴇ]
-◌ /boobs [ᴄʜᴇᴄᴋ ʏᴏᴜʀ ʙᴏᴏʙɪᴇs sɪᴢᴇ]
-◌ /dick [ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴅɪᴄᴋ sɪᴢᴇ]
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /sigma [ᴄʜᴇᴄᴋ ʏᴏᴜʀ sɪɢᴍᴀɴᴇss]
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /cute [ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜᴛᴇɴᴇss]
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /horny [ᴄʜᴇᴄᴋ ʏᴏᴜʀ ʜᴏʀɴʏɴᴇss]
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /lesbo [ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ʟᴇᴢʙɪᴀɴ ʏᴏᴜ ᴀʀᴇ]
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /depressed [ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ᴅᴇᴘʀᴇssᴇᴅ ʏᴏᴜ ᴀʀᴇ]
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /gay [ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ɢᴀʏ ʏᴏᴜ ᴀʀᴇ]
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /rand [ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ʀᴀɴᴅ ʏᴏᴜ ᴀʀᴇ]
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /bkl [ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ʙᴋʟ ʏᴏᴜ ᴀʀᴇ]
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /boobs [ᴄʜᴇᴄᴋ ʏᴏᴜʀ ʙᴏᴏʙɪᴇs sɪᴢᴇ]
+<tg-emoji emoji-id="6170455814810112778">💖</tg-emoji> /dick [ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴅɪᴄᴋ sɪᴢᴇ]
 '''
-    HELP_Q = '''ǫᴜᴏᴛʟʏ
+    HELP_Q = '''<tg-emoji emoji-id="6021618194228187816">💬</tg-emoji> <b>ǫᴜᴏᴛʟʏ</b>
 
 ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ғᴏʀ ᴛʜᴇ ǫᴜᴏᴛʟʏ ᴍᴏᴅᴜʟᴇ:
 
-◌ /q : ᴄʀᴇᴀᴛᴇ ᴀ ǫᴜᴏᴛᴇ ғʀᴏᴍ ᴛʜᴇ ᴍᴇssᴀɢᴇ
+<tg-emoji emoji-id="5370546867786523009">📝</tg-emoji> /q : ᴄʀᴇᴀᴛᴇ ᴀ ǫᴜᴏᴛᴇ ғʀᴏᴍ ᴛʜᴇ ᴍᴇssᴀɢᴇ
 
-◌ /q r : ᴄʀᴇᴀᴛᴇ ᴀ ǫᴜᴏᴛᴇ ғʀᴏᴍ ᴛʜᴇ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ʀᴇᴘʟʏ
+<tg-emoji emoji-id="5370546867786523009">📝</tg-emoji> /q r : ᴄʀᴇᴀᴛᴇ ᴀ ǫᴜᴏᴛᴇ ғʀᴏᴍ ᴛʜᴇ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ʀᴇᴘʟʏ
 '''
     
     
